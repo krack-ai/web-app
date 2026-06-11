0e66f90c5a3f5de49a39a518d83dc7f969d511d5
JonathanRaju <sirigirijonathanraju@gmail.com>
2026-06-11 20:37:38 +0530

    updated download links

--- app/api/download/mac/route.ts
+++ app/api/download/mac/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.redirect(
-    "https://github.com/JonathanRaju/overlay-ai/releases/download/v1.0.0/Krack-AI.dmg"
+    "https://github.com/krack-ai/web-app/releases/download/1.1/Krack-AI.dmg"
   );
 }
--- app/api/download/windows/route.ts
+++ app/api/download/windows/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.redirect(
-    "https://github.com/JonathanRaju/overlay-ai/releases/download/v1.0.0/overlay-ai.exe"
+    "https://github.com/krack-ai/web-app/releases/download/1.1/overlay-ai.exe"
   );
 }
